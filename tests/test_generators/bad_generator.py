d9cc2292ef10a4aa7f054d30eb9d56cfc2986284
Ronald Kaczynski <ronald.kaczynski@laerdal.com>
2025-10-23 11:18:09 +0200

    include .py files for testing

---
+++ tests/test_generators/bad_generator.py
@@ -0,0 +1,5 @@
+
+from objdictgen.typing import NodeProtocol, TPath
+
+def Generate_File(filepath: TPath, node: NodeProtocol):
+    return ""
---
+++ tests/test_generators/generator.py
@@ -0,0 +1,5 @@
+
+from objdictgen.typing import NodeProtocol, TPath
+
+def GenerateFile(filepath: TPath, node: NodeProtocol):
+    return "success"
